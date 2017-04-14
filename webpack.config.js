37833a22e2b6ef4476cb964866de8cb95faecb4c
Helen Gurina <Helen@Helens-MacBook-Air.local>
2017-04-14 12:18:11 -0600

    Webpack and style updates

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,11 @@ module.exports = {
   },
   module: {
     loaders: [{
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel',
       query: {
+        cacheDirectory: true,
         presets: ['react', 'es2015', 'stage-1']
       }
     }]
